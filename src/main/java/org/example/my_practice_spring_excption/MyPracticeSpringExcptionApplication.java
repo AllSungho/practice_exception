0c600ab3bf88a00fe6daf2137a9f745d0ae2d043
AllSungho <nanumeran@gmail.com>
2025-08-22 23:58:32 +0900

    first commit

---
+++ src/main/java/org/example/my_practice_spring_excption/MyPracticeSpringExcptionApplication.java
@@ -0,0 +1,13 @@
+package org.example.my_practice_spring_excption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPracticeSpringExcptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPracticeSpringExcptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/my_practice_spring_excption/MyPracticeSpringExcptionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.my_practice_spring_excption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPracticeSpringExcptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
