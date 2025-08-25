d66a878fd266bd2eb737c2661302458b1bfab553
AllSungho <nanumeran@gmail.com>
2025-08-25 09:49:47 +0900

    Create NotValidException

--- src/main/java/org/example/my_practice_spring_exception/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/my_practice_spring_exception/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package org.example.my_practice_spring_exception.common.exception;
 
 import org.example.my_practice_spring_exception.common.exception.custom.NoSessionException;
+import org.example.my_practice_spring_exception.common.exception.custom.NotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,5 +19,15 @@ public class GlobalExceptionHandler {
                 HttpStatus.UNAUTHORIZED.value()
         );
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+
+    @ExceptionHandler(NotValidException.class)
+    public ResponseEntity<ErrorResponse> handleNotValidException(NotValidException e) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                e.getErrorCode(),
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
